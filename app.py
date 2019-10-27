c171074766928a8cc04722e0a2e7a5fab86de179
karan <kxm9436@rit.edu>
2019-10-27 16:47:58 -0400

    modified query 2

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import render_template
 import pypyodbc
 
 
-
 app = Flask(__name__)
 
 cnxnStr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-3BKTPOD;DATABASE=yelp;Trusted_Connection=yes;'
@@ -52,7 +51,7 @@ def q2():
         details = request.args
         btype = details['type']
         bzip = details['zip']
-        percent = details['percent']
+        percent = int(details['percent'])/100
         rating = details['rating']
 
         # cur = mysql.connection.cursor()
@@ -61,14 +60,14 @@ def q2():
         cnxn = pypyodbc.connect(cnxnStr)
         cur = cnxn.cursor()
         cur.execute(
-            """SELECT tab1.name from  ( SELECT b1.name, b1.business_id ,count(rev1.r_id) as count1 from businessTable as b1 join restaurantTable as r1 on b1.business_id = r1.business_id join reviewTable as rev1 on b1.business_id = rev1.b_id where b1.postal_code='89121' group by b1.business_id,b1.name) as tab1
+            """SELECT tab1.name from  ( SELECT b1.name, b1.business_id ,count(rev1.r_id) as count1 from businessTable as b1 join {0} as r1 on b1.business_id = r1.business_id join reviewTable as rev1 on b1.business_id = rev1.b_id where b1.postal_code='{1}' group by b1.business_id,b1.name) as tab1
 
 join
 
-( SELECT b2.name, b2.business_id ,count(rev2.r_id) as count2 from businessTable as b2 join restaurantTable as r2 on b2.business_id = r2.business_id join reviewTable as rev2 on b2.business_id = rev2.b_id where b2.postal_code='89121' and rev2.stars>3 group by b2.business_id,b2.name) as tab2
+( SELECT b2.name, b2.business_id ,count(rev2.r_id) as count2 from businessTable as b2 join {0} as r2 on b2.business_id = r2.business_id join reviewTable as rev2 on b2.business_id = rev2.b_id where b2.postal_code='{1}' and rev2.stars>{3} group by b2.business_id,b2.name) as tab2
 
-ON tab1.business_id=tab2.business_id WHERE count2/count1 >0.5;;"""
-            .format(name, reviews))
+ON tab1.business_id=tab2.business_id WHERE count2/count1 >{2};"""
+            .format(btype,bzip,percent,rating))
         data = cur.fetchall()
         return render_template("q2table.html", data=[btype, bzip, percent, rating])
 
