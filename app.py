edcefb10133c34d633db545b9f3cdc880f111e74
karan <kxm9436@rit.edu>
2019-10-27 18:32:14 -0400

    Merge branch 'master' of C:\Users\karan\PycharmProjects\IBDproject with conflicts.

--- app.py
+++ app.py
@@ -91,10 +91,21 @@ def q4():
         details = request.args
         key = details['userid']
 
-        # cur = mysql.connection.cursor()
-        # cur.execute("""select name from business_table2 where postal_code ='{zip}' and business_id in (select b_id from reviewTable group by b_id having avg(b_id)>{rating});""")
-        # data = cur.fetchall()
-        return render_template("q4table.html", data=[key])
+        cnxn = pypyodbc.connect(cnxnStr)
+        cur = cnxn.cursor()
+        cur.execute(
+            """SELECT tab1.name from
+
+(SELECT b1.name, b1.business_id,count(rev1.r_id) as count1 from friendsTable as f1 join userTable as u1 on f1.u_id=u1.u_id join reviewTable as rev1 on f_id=rev1.u_id join businessTable as b1 on rev1.b_id=b1.business_id join restaurantTable as r1 on b1.business_id=r1.business_id where rev1.stars>4  and u1.u_id='{0}' group by b1.business_id,b1.name) as tab1
+
+join
+
+(SELECT b2.name,b2.business_id,count(rev2.r_id) as count2 from friendsTable as f2 join userTable u2 on f2.u_id=u2.u_id join reviewTable as rev2 on f_id=rev2.u_id join businessTable as b2 on rev2.b_id=b2.business_id join restaurantTable as r2 on b2.business_id=r2.business_id where rev2.stars<3 and u2.u_id='{0}'  group by b2.business_id,b2.name) as tab2
+
+ON tab1.business_id=tab2.business_id WHERE count1>count2 ;;"""
+                .format(key))
+        data = cur.fetchall()
+        return render_template("q4table.html", data=data)
 
 
 @app.route('/q5', methods=['GET', 'POST'])
